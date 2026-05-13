42c28c4f75b1447c51c5e4da5891c378b0d8b359
Atadeniz Bektaş <atadenizbkts@gmail.com>
2026-05-13 10:55:50 +0300

    added options for sample rate and channel count

--- convert.py
+++ convert.py
@@ -9,6 +9,9 @@ def convert():
         outputFile = input("Output file: ") 
         if (outputFile.lower() =="q"):
             return
+        
+        #bitrate handling
+        isBitrate = False
         bitrate = input("bitrate (optional): ")
         if (bitrate.lower()=="q"):
             return
@@ -18,22 +21,48 @@ def convert():
             except:
                 print("You can only type numbers for bitrate\n")
                 continue
-                
-        if not bitrate:
+            isBitrate=True
+        
+        #sample rate handling
+        isSamplerate = False
+        samplerate = input("sample rate (Hz) (optional): ")
+        if (samplerate.lower()=="q"):
+            return
+        elif samplerate.strip()!="":
             try:
-                process = subprocess.run(["ffmpeg","-i","assets/" + inputFile, outputFile], check=True,stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
-                return
-            except subprocess.CalledProcessError:
-                print("\nYou sure you typed your input or output file name right? Try again.\n")
-            except FileNotFoundError:
-                print("\nMake sure you have downloaded ffmpeg to your device and added it to path.\n")
-
-              
-        else:    
-            try:
-               process = subprocess.run(["ffmpeg","-i","assets/" + inputFile, "-b:a", bitrate +"k", outputFile], check=True,stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
-               return 
-            except subprocess.CalledProcessError:
-                print("\nYou sure you typed your input or output file name right? Try again.\n")
-            except FileNotFoundError:
-              print("\nMake sure you have downloaded ffmpeg to your device and added it to path.\n")
+                int(samplerate) 
+            except:
+                print("You can only type numbers for sample rate\n")
+                continue
+            isSamplerate=True
+            
+        #channels
+        isChannel=False
+        channel = input("channels (1 for mono, 2 for stereo) (optional): ")
+        if (channel.lower()=="q"):
+            return
+        elif (int(channel) not in (1,2)):
+            print("\nPlease type 1 for mono and 2 for stereo\n")
+            continue
+        elif channel.strip()!="": 
+            isChannel=True
+            
+        #properties container
+        properties = {"-b:a":isBitrate, bitrate+"k":isBitrate,"-ac":isChannel,channel:isChannel, "-ar":isSamplerate,samplerate:isSamplerate}
+        
+        command = ["ffmpeg","-i","assets/" + inputFile]
+        
+        #assign options
+        for key,value in properties.items():
+            if value==True:
+                command.append(key)
+        
+        #finalize command
+        command.append(outputFile)
+        try:
+            process = subprocess.run(command, check=True,stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+            return
+        except subprocess.CalledProcessError:
+            print("\nYou sure you typed your input or output file name right? Try again.\n")
+        except FileNotFoundError:
+            print("\nMake sure you have downloaded ffmpeg to your device and added it to path.\n")
